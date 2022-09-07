a70c0113b0d165bd42dce1d70cecc1160239e93a
Dean <dean.marinov@gmx.at>
2022-09-07 21:53:59 +0200

    füge key generator hinzu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -89,5 +89,55 @@ namespace WpfApp1
 			ausgabeBottomR.Text = sharedSecretKeyAlice.ToString();
 			ausgabeBottomR1.Text = versuche.ToString();
 		}
+
+		private void BtnCreateKey(object sender, RoutedEventArgs e)
+		{
+			mpz_t privateKeyAlice = new mpz_t();
+			mpz_t privateKeyBob = new mpz_t();
+			mpz_t sharedSecretKeyAlice = new mpz_t();
+			mpz_t sharedSecretKeyBob = new mpz_t();
+			mpz_t exchangeKeyAlice = new mpz_t();
+			mpz_t exchangeKeyBob = new mpz_t();
+			gmp_randstate_t rnd = new gmp_randstate_t();
+
+			//init
+			gmp_lib.gmp_randinit_default(rnd);
+			gmp_lib.mpz_init(privateKeyAlice);
+			gmp_lib.mpz_init(privateKeyBob);
+			gmp_lib.mpz_init(exchangeKeyAlice);
+			gmp_lib.mpz_init(exchangeKeyBob);
+			gmp_lib.mpz_init(exchangeKeyAlice);
+			gmp_lib.mpz_init(exchangeKeyBob);
+			gmp_lib.mpz_init(sharedSecretKeyAlice);
+			gmp_lib.mpz_init(sharedSecretKeyBob);
+
+			uint modulo = gmp_lib.gmp_urandomb_ui(rnd, 32);
+			uint alicePrivate = gmp_lib.gmp_urandomb_ui(rnd, 8);
+			uint basis = gmp_lib.gmp_urandomb_ui(rnd, 5);
+			uint bobPrivate = gmp_lib.gmp_urandomb_ui(rnd, 8);
+
+			generateAlicePublic.Text = modulo.ToString();
+			generateBobPublic.Text = basis.ToString();
+			generateAlicePrivate.Text = alicePrivate.ToString();
+			generateBobPrivate.Text = bobPrivate.ToString();
+
+			gmp_lib.mpz_ui_pow_ui(exchangeKeyAlice, basis, alicePrivate);
+			gmp_lib.mpz_mod_ui(exchangeKeyAlice, exchangeKeyAlice, modulo);			
+			
+			gmp_lib.mpz_ui_pow_ui(exchangeKeyBob, basis, alicePrivate);
+			gmp_lib.mpz_mod_ui(exchangeKeyBob, exchangeKeyBob, modulo);
+
+			this.exchangeKeyAlice.Text = exchangeKeyAlice.ToString();
+			this.exchangeKeyBob.Text = exchangeKeyBob.ToString();
+
+			gmp_lib.mpz_pow_ui(sharedSecretKeyAlice, exchangeKeyBob, alicePrivate);
+			gmp_lib.mpz_mod_ui(sharedSecretKeyAlice, sharedSecretKeyAlice, modulo);
+
+			gmp_lib.mpz_pow_ui(sharedSecretKeyBob, exchangeKeyAlice, bobPrivate);
+			gmp_lib.mpz_mod_ui(sharedSecretKeyBob, sharedSecretKeyBob, modulo);
+
+			sharedSecretKeyAliceBox.Text = sharedSecretKeyAlice.ToString();
+			sharedSecretKeyBobBox.Text = sharedSecretKeyBob.ToString();
+		}
 	}
 }
